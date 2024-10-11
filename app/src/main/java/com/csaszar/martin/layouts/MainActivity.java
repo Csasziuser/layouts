e3ac2fc224951dba1f5515e3495065551d82b849
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2024-10-11 08:58:25 +0200

    ora kozben1

--- app/src/main/java/com/csaszar/martin/layouts/MainActivity.java
+++ app/src/main/java/com/csaszar/martin/layouts/MainActivity.java
@@ -1,6 +1,9 @@
 package com.csaszar.martin.layouts;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button linear = findViewById(R.id.btnToLinear);
+        Button relative = findViewById(R.id.btnToRelative);
+        Button frame = findViewById(R.id.btnToFrame);
+        Button table = findViewById(R.id.btnToTable);
+        Button web = findViewById(R.id.btnToWeb);
+
+        linear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentToLinear = new Intent(MainActivity.this,LinearActivity.class);
+                startActivity(intentToLinear);
+            }
+        });
+
+        relative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentToRelative = new Intent(MainActivity.this, RelativeActivity.class);
+                startActivity(intentToRelative);
+            }
+        });
+
+
+
+
     }
 }
